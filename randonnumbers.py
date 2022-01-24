c5f582b5abd0362dad86bddf4bdf2f70c19902b4
tamiressantosdata <93625492+tamiressantosdata@users.noreply.github.com>
2022-01-24 20:31:27 -0300

    Add files via upload

---
+++ randonnumbers.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+
+
+
+data= {i: np.random.randn() for i in range(7)} 
+print(data)
+
+from numpy.random import randn
+data = {i: randn() for i in range (7)} 
+
+
