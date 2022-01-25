e4853a50fd7958e85fe1caf942ee7c0c823d910a
tamiressantosdata <93625492+tamiressantosdata@users.noreply.github.com>
2022-01-25 18:43:58 -0300

    Add files via upload

---
+++ for_break.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 18:14:15 2022
+
+@author: tamir
+"""
+
+for i in range(4):
+    for j in range(4):
+        if j > i:
+            break
+        print((i, j))
+        
+
