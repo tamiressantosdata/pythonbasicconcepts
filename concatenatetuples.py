ee158ddc30c9059078ed62a737e3b93e2c3efec0
Tamires <93625492+tamiressantosdata@users.noreply.github.com>
2022-03-25 18:27:51 -0300

    Add files via upload

---
+++ concatenatetuples.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 25 18:24:28 2022
+
+@author: tamir
+"""
+
+t1 = (1,2,3,4)
+t2 = (5,6,7,8)
+
+print(t1 + t2)
+
+#However, although they don’t support resizing, we can concatenate two tuples together
+#and form a new tuple. The operation is similar to the resize operation on lists, but we
+#do not allocate any extra space for the resulting tuple
---
+++ tuplestaticarrays.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 25 18:18:10 2022
+
+@author: tamir
+"""
+
+t = (1,2,3,4)
+t[0]=5
+
+t1 = (1,2,3,4)
+t2 = (5,6,7,8)
+
+print(t1 + t2)
