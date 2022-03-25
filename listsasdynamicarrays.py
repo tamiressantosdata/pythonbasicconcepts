fdddce32eae1d42e2c0fa00ad6accb379c19b706
Tamires <93625492+tamiressantosdata@users.noreply.github.com>
2022-03-25 18:11:55 -0300

    Add files via upload

---
+++ listsasdynamicarrays.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 25 18:04:50 2022
+
+@author: tamir
+"""
+
+numbers = [5, 8, 1, 3, 2, 6]
+numbers[2] = 2*numbers[0]
+numbers
+len(numbers)
+print(len(numbers))
+numbers.append(42)
+print(len(numbers))
